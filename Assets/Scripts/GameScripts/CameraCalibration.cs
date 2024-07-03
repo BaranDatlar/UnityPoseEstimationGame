51b6cdfcea3a006dc03e22d7f32f1e80f740726c
BaranDatlar <barandatlarr@gmail.com>
2024-07-04 00:17:19 +0300

    new level prefab added and coundown animation implemented

--- Assets/Scripts/GameScripts/CameraCalibration.cs
+++ Assets/Scripts/GameScripts/CameraCalibration.cs
@@ -10,12 +10,15 @@ public class CameraCalibration : MonoBehaviour
     public Button startCalibration;
     public Button skipCalibration;
     public TextMeshProUGUI countDownText;
-    public int countDownValue = 10;
+    public int countDownValue = 5;
     public float averageYCoordinate;
     public bool calibrationCompleted;
 
     public static CameraCalibration instance { get; private set; }
     public UnityEvent onCalibrationFinished;
+    public Canvas calibrationCanvas;
+
+    private Vector3 startScale = new Vector3(1, 1, 1);
 
     private void Awake()
     {
@@ -37,6 +40,24 @@ public class CameraCalibration : MonoBehaviour
         skipCalibration.onClick.AddListener(SkipCalibration);
     }
 
+    IEnumerator ScaleOverTime(float time, TextMeshProUGUI countDownText, Canvas calibrationCanvas)
+    {
+        float currentTime = 0.0f;
+        Color originalCanvasColor = calibrationCanvas.GetComponent<Image>().color;
+        while (currentTime <= time)
+        {
+            countDownText.transform.localScale = Vector3.Lerp(startScale, startScale * 5f, currentTime / time);
+            originalCanvasColor.a = Mathf.Lerp(0, 0.5f, currentTime / time);
+            calibrationCanvas.GetComponent<Image>().color = new Color(originalCanvasColor.r, originalCanvasColor.g, originalCanvasColor.b, originalCanvasColor.a);
+            currentTime += Time.deltaTime;
+            yield return null; // Bir sonraki frame'i bekle
+        }
+        countDownText.transform.localScale = startScale;
+        originalCanvasColor.a = 0;
+        calibrationCanvas.GetComponent<Image>().color = originalCanvasColor;
+
+    }
+
     IEnumerator StartCountdown()
     {
         countDownText.gameObject.SetActive(true);
@@ -45,13 +66,14 @@ public class CameraCalibration : MonoBehaviour
 
         while (countDownValue > 0)
         {
-            if (countDownValue <= 5)
+            if (countDownValue <= 3)
             {
                 float currentYCoordinate = BodyPartAngles.ReturnMidPointOfShoulders().y;
                 YCoordinates.Add(currentYCoordinate);
             }
 
             countDownText.text = countDownValue.ToString();
+            StartCoroutine(ScaleOverTime(1, countDownText,calibrationCanvas));
             yield return new WaitForSeconds(1);
             countDownValue--;
         }
